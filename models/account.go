07aca4978c823830e53286d0c0b8c9008e3c3a60
Bobby <30593201+luciferreeves@users.noreply.github.com>
2026-09-05 19:56:09 +0530

    feat: give an account an email address

--- models/account.go
+++ models/account.go
@@ -5,5 +5,6 @@ import "gorm.io/gorm"
 type Account struct {
 	gorm.Model
 	Username     string `gorm:"uniqueIndex;not null"`
+	Email        string `gorm:"uniqueIndex;not null"`
 	PasswordHash string `gorm:"not null"`
 }
